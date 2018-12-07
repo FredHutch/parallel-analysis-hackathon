79c38bbc4f80cf5b9cb89a30697d143249a6235b
Dev Nambi <dnambi@fredhutch.org>
2018-12-06 19:37:33 -0800

    Updating bootstrap script with permissions change

--- boostrap-scripts/startup.sh
+++ boostrap-scripts/startup.sh
@@ -18,25 +18,25 @@ fi
 
 echo "Installing basic packages"
 #install R
-yum install -y nano curl
-amazon-linux-extras install -y R3.4
+sudo yum install -y nano curl
+sudo amazon-linux-extras install -y R3.4
 
 
 
 echo "Creating working directories"
-mkdir -p /var/analysis
+sudo mkdir -p /var/analysis
 
 LOCAL_INPUT_DIR="/var/analysis/input/"
 LOCAL_OUTPUT_DIR="/var/analysis/output/"
-mkdir -p $LOCAL_INPUT_DIR
-mkdir -p $LOCAL_OUTPUT_DIR
+sudo mkdir -p $LOCAL_INPUT_DIR
+sudo mkdir -p $LOCAL_OUTPUT_DIR
 
 
 
 echo "Getting the R script"
 RFILELOCATION="/var/analysis/r-script.r"
 if [ -f $RFILELOCATION ] ; then
-    rm -f $RFILELOCATION
+    sudo rm -f $RFILELOCATION
 fi
 
 
@@ -44,24 +44,25 @@ fi
 echo "Getting the R script from S3"
 # get the file from S3
 echo 
-aws s3 cp "${R_SCRIPT_S3_URL}" "${RFILELOCATION}" || error_exit "Failed to download R script from S3"
+sudo aws s3 cp "${R_SCRIPT_S3_URL}" "${RFILELOCATION}" || error_exit "Failed to download R script from S3"
 
 
 
 echo "Getting the input data from S3"
 # get the file from S3
 echo 
-aws s3 cp "${INPUT_S3_DIR}" "${LOCAL_INPUT_DIR}" || error_exit "Failed to download R script from S3"
+sudo aws s3 cp "${INPUT_S3_DIR}" "${LOCAL_INPUT_DIR}" --recursive || error_exit "Failed to download input data from S3"
 
 
 
 echo "Running the R script"
-R -f $RFILELOCATION
+sudo R -f $RFILELOCATION
 
 
 
 echo "Saving the results back to S3"
-#aws s3 cp 
+sudo aws s3 cp "${LOCAL_OUTPUT_DIR}" "${S3_OUTPUT_DIR}" --recursive || error_exit "Failed to upload results to S3"
+
 
 
 date

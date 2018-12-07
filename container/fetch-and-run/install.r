9dc8945e78541b12476a4a8912758237d92a15b8
Dev Nambi <dnambi@fredhutch.org>
2018-12-07 11:56:02 -0800

    Updated notebooks making them simpler. Updated Dockerfile

--- container/fetch-and-run/install.r
+++ container/fetch-and-run/install.r
@@ -1,3 +1,7 @@
 install.packages("Rcpp", repo="https://cloud.r-project.org/")
+install.packages("devtools", repo="https://cloud.r-project.org/")
+library(devtools)
+devtools::install_github("RGLab/scamp")
+
 intro <- "R is now installed and running"
 intro

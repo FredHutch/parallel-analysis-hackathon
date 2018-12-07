aabeb80bf180d1aff884bbfbeab07bceeff5b7f3
Evan Greene <egreene@fredhutch.org>
2018-12-07 13:04:34 -0800

    Param change.

--- Rscript/processData.R
+++ Rscript/processData.R
@@ -4,6 +4,7 @@ print(paste0("Number of columns: ", ncol(scampData)))
 print(paste0("Number of rows: ", nrow(scampData)))
 print("Starting SCAMP")
 scampOutput <- scamp(dataSet=scampData,
+                     numberIterations = 1,
                      numberOfThreads=16)
 print("SCAMP complete.")
 print("Writing output.")
